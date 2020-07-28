8bf80f1b6595ffcb8ff9f5be7bce871ff1233817
mariafrancis <maria.francis193@gmail.com>
2020-07-28 18:22:11 +0530

    first commit

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<stdlib.h>
+int main()
+{
+
+    system('cls');
+    int a,b;
+    cout<<"enter 2 numbers";
+    cin>>a>>b;
+    cout<<"the sum is " <<a+b;
+    return 0;
+}
